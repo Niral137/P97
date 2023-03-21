7db1ec4bd022badf1121ead28e1e5834fa5cb2d2
Niral137 <88650779+Niral137@users.noreply.github.com>
2023-03-21 10:14:11 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function pageOne(){
+    window.location = "chicken_noodle_soup.html";
+}
+function pageTwo(){
+    window.location = "sweet_corn_soup.html";
+}
+function pageThree(){
+    window.location = "manchow_soup.html";
+}
+
+
